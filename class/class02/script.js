fa3ee13e43311ede1de510b1192ba24c7a43daed
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-07-17 10:57:15 +0530

    private class fields

--- class/class02/script.js
+++ class/class02/script.js
@@ -39,3 +39,32 @@ console.log(Point.distance(point_01, point_02))
 // to samajhne ki baat ye hai ki "static" properties class ke liye hi defined hai 
 // uske instances ke liye nahi...
 // console.log(point_01.displayName) --> undefined  :-)
+
+
+/* Field Declarations: 
+* Field declarations can either be (1) public class field or (2) private class field
+*  Public class fields can be accessed from both inside and outside of the class instances
+*  Private class fields can be accessed within the class itself.
+*   Private class fields are declared with a prefixed # symbol:::
+*/
+
+class MyClass{
+    publicField = "I am a public field";
+    #privateField = "I am a private field";
+
+    myMethod() {
+        console.log("public: "+ this.publicField);
+        console.log("private: "+ this.#privateField);
+    }
+}
+
+const myInstance = new MyClass();
+
+console.log(myInstance.publicField);
+myInstance.myMethod();
+// console.log(myInstance.#privateField); it will throw a syntax errror:
+
+/* It's an error to reference private fields from outside of the class; 
+they can only be read or written within the class body. 
+By defining things that are not visible outside of the class, you ensure that 
+your classes' users can't depend on internals, which may change from version to version. */
