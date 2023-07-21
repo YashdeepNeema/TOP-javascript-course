802bc6bf117addd680c80bb1fc9b917f7d2f04aa
Yashdeep Neema <yashdeepneema@hotmail.com>
2023-07-21 12:03:05 +0530

    do class examples from JS The Definitive Guide

---
+++ class/class06/script02.js
@@ -0,0 +1,44 @@
+/**
+ * Example 9.6 of JS A definitive Guide:
+ */
+
+class TypeMap extends Map{
+    constructor(keyType, valueType, entries){
+        // If entries are specified, check their types: 
+        //{pahale entries to check kar liya}
+        if(entries){
+            for(let [k,v] of entries){
+                if (typeof k !== keyType || typeof v !== valueType){
+                    throw new TypeError(`Wrong type for entry [${k}, ${v}]`);
+                }
+            }
+        }
+
+        //Initialize the superclass with the (type-checked) initial entries
+        //{Ab entries check ho gayi to superclass to initialize kar liya ... pr kyo ??}
+        super(entries);
+
+
+        //And then initialize this subclass by storing the types:
+        this.keyType = keyType;
+        this.valueType = valueType;
+    }
+
+    // Now, redefine the set() method to add type checking for any new entries added to the map.
+
+    set(key, value){
+        // Throw an error if the key or value are of the wrong type:
+        if(this.keyType && typeof key !== this.keyType){
+            throw new TypeError(`${key} is not of the type ${this.keyType}`);
+        }
+        if(this.valueType && typeof value !== this.valueType){
+            throw new TypeError(`${value} is not of type ${this.valueType}`);
+        }
+
+        // If the types are correct, we invoke the superclass's version of the set method,
+        // to actually add the entry to the map. 
+        // And we return whatever the superclass method returns:
+
+        return super.set(key, value);
+    }
+}
